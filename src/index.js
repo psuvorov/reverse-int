a348639d4748783b3b4564fe37d9e0354e822aec
PavelSuvorov <psuvorov@riverlogic.com>
2021-02-22 00:03:26 +0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function reverse (n) {
-  
+  return rev(n);
+}
+
+/**
+ * 
+ * @param {number} n
+ */
+function rev(n) {
+    n = Math.abs(n);
+    
+    let t = 1;
+    let l = 0;
+    while (t <= n) {
+        l++;
+        t *= 10;
+    }
+    
+    let res = 0;
+    while (n > 0) {
+        let r = n % 10;
+        res += r * (10 ** --l);  
+        
+        n = Math.trunc(n / 10);
+    }
+    
+    return res;
 }
